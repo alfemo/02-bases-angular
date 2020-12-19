78d7eb6c0d7bce6ed41ca88591c5bdefa9b87264
Alvaro Mora <mora_alvaro@yahoo.com.ar>
2020-12-19 17:59:26 -0300

    [ADD] Sección3. Cierre.

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-posts',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  mensajes: any;
+
+  constructor( private dataService: DataService ) { }
 
   ngOnInit(): void {
+
+    this.mensajes = this.dataService.getPosts();
+        // .subscribe(
+        //   posts => {
+        //     console.log(posts);
+        //     this.mensajes = posts;
+        //   }
+        // )
+  }
+
+  escuchaClick( event: number ) {
+    console.log("Click en: " + event);
   }
 
 }
